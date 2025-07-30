c2947af621b5d1592d38562794a0b6ca1c8ab8b9
fffirer <0sdf0@sina.cn>
2025-07-30 23:50:57 +0800

    add: test toolchain

---
+++ tests/acme.tests/SignFlowUnitTests.cs
@@ -0,0 +1,14 @@
+using System;
+
+using AutoFixture;
+
+namespace CertsServer.Acme.Tests;
+
+public class SignFlowUnitTests
+{
+    [Fact]
+    public Task Sign_Except_Success()
+    {
+        return Task.CompletedTask;
+    }
+}
