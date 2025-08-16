4cf6acbc20885b6329f40bd806aed60faf3e0e3e
fffirer <0sdf0@sina.cn>
2025-08-17 07:36:28 +0800

    fix: efcore migrations

--- sqlite-migrations/Migrations/20250816233622_Update_TicketOrder_Expires_Conversion.Designer.cs
+++ sqlite-migrations/Migrations/20250816233622_Update_TicketOrder_Expires_Conversion.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CertsServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CertsServer.Migrations
 {
     [DbContext(typeof(CertsServerDbContext))]
-    partial class CertsServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250816233622_Update_TicketOrder_Expires_Conversion")]
+    partial class Update_TicketOrder_Expires_Conversion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.0-preview.5.25277.114");
---
+++ sqlite-migrations/Migrations/20250816233622_Update_TicketOrder_Expires_Conversion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CertsServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_TicketOrder_Expires_Conversion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Expires",
+                table: "ticket_orders",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Expires",
+                table: "ticket_orders",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "INTEGER",
+                oldNullable: true);
+        }
+    }
+}
